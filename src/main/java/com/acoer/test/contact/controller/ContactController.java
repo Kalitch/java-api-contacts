2bfd1b2a3b05cb9d2e20e1b49469b5d47d150100
Marcelo <104369724+MarceloCastroSF@users.noreply.github.com>
2022-05-02 14:29:06 -0300

    fix: fix semantic release

--- src/main/java/com/acoer/test/contact/controller/ContactController.java
+++ src/main/java/com/acoer/test/contact/controller/ContactController.java
@@ -29,7 +29,7 @@ public class ContactController {
 
 	private static final Logger logger = LogManager.getLogger();
 
-	// getall
+	// getall v2
 	@RequestMapping(method = RequestMethod.GET, value = "/list", produces = MediaType.APPLICATION_JSON_VALUE)
 	@ApiOperation(value = "Get all contacts", notes = "Get all contacts")
 	public ResponseEntity<List<Contact>> getContacts() {
